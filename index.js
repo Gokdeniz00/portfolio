aa2296c8535a862ee2f51a3379db5bcfabd5e384
Gökdeniz Dağ <mountainskysea07@gmail.com>
2024-02-14 14:16:59 +0200

    Finished back end

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express =require('express');
 const app= express();
+const fs = require('fs');
 
-//app.get("/")
+app.get("/.", (req,res)=>{
+    if(req.path.match("/")){
+        data=fs.readFile("/contents/index.htm")
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.send(data)
+        res.end()
+    }else if(req.path.match("/certs")){
+        data=fs.readFile("/contents/certs.htm");
+        res.writeHead(200,{'Content-Type': 'text/html'})
+        res.send(data)
+        res.end()
+    }else{
+        data=fs.readFile("/contents/404.html");
+        res.writeHead(404,{'Content-Type': 'text/html'})
+        res.send(data)
+        res.end()
+    }
+})
